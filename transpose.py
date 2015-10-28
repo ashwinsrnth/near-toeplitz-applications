ef87a71bdac515f25aa6b4f1bd9f9fb7fcd25782
Ashwin Srinath <ashwinsrnth@gmail.com>
2015-10-27 20:02:59 -0400

    add transpose code

--- transpose.py
+++ transpose.py
@@ -3,12 +3,13 @@ from pycuda import autoinit
 import pycuda.driver as cuda
 import pycuda.compiler as compiler
 import pycuda.gpuarray as gpuarray
+from pycuda.tools import context_dependent_memoize, clear_context_caches
 import jinja2
 
 kernel_template = """
 __global__ void transposeCoalesced(double *odata, const double *idata)
 {
-  __shared__ double tile[{{TILE_DIM}}][{{TILE_DIM}}];
+  __shared__ double tile[{{TILE_DIM}}][{{TILE_DIM+1}}];
 
   int x = blockIdx.x * {{TILE_DIM}} + threadIdx.x;
   int y = blockIdx.y * {{TILE_DIM}} + threadIdx.y;
@@ -26,18 +27,26 @@ __global__ void transposeCoalesced(double *odata, const double *idata)
      odata[(y+j)*width + x] = tile[threadIdx.x][threadIdx.y + j];
 }
 """
+@context_dependent_memoize
+def _get_transpose_kernel(block_size):
+    TILE_DIM = block_size[0]
+    BLOCK_ROWS = block_size[1]
+    tpl = jinja2.Template(kernel_template)
+    rendered_kernel = tpl.render(TILE_DIM=TILE_DIM, BLOCK_ROWS=BLOCK_ROWS)
+    module = compiler.SourceModule(rendered_kernel, options=['-O2'])
+    transposeFunc = module.get_function('transposeCoalesced')
+    transposeFunc.prepare('PP')
+    return transposeFunc
 
-tpl = jinja2.Template(kernel_template)
-rendered_kernel = tpl.render(TILE_DIM=32, BLOCK_ROWS=8)
-module = compiler.SourceModule(rendered_kernel, options=['-O2'])
-transposeFunc = module.get_function('transposeCoalesced')
-transposeFunc.prepare('PP')
+def transpose(a, b, N):
+    '''
+    Transpose the array a,
+    putting the result in b.
+    a is assumed square with
+    width N---multiple of 32
+    '''
+    block_size = (32, 32)
+    f = _get_transpose_kernel(block_size)
+    f.prepared_call((N/block_size[0], N/block_size[0], 1), (block_size[0], block_size[1], 1),
+                b.gpudata, a.gpudata)
 
-a = np.random.rand(64, 64)
-b = np.zeros((64, 64), dtype=np.float64)
-a_gpu = gpuarray.to_gpu(a)
-b_gpu = gpuarray.to_gpu(b)
-transposeFunc.prepared_call((2, 2, 1), (32, 8, 1), b_gpu.gpudata, a_gpu.gpudata)
-
-from numpy.testing import *
-assert_allclose(a.transpose(), b_gpu.get())
